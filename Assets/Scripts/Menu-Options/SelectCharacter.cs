73fdb39e8a700b2f480985ea59c64fe119ac3c83
jysique <joseysique@gmail.com>
2021-03-23 20:51:33 -0500

    Menu system: connection menu-dialogueSystem

---
+++ Assets/Scripts/Menu-Options/SelectCharacter.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class SelectCharacter : MonoBehaviour
+{
+    Button select;
+
+    void Start()
+    {
+        select = GetComponent<Button>();
+        select.onClick.AddListener(() => ChangeScene());
+    }
+    void ChangeScene()
+    {
+        SceneManager.LoadScene("VisualNovelScene");
+    }
+}
